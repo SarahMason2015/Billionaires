9d014ca8592f7ac9eee12897db09d8e9ed35b64c
Ben Charlton <93561592+BHCharlton@users.noreply.github.com>
2022-05-18 19:34:17 -0500

    Add files via upload

--- Cleaning/Billionaires_Database.sql
+++ Cleaning/Billionaires_Database.sql
@@ -1,1 +1,22 @@
-CREATE TABLE 
+CREATE TABLE Billionaires_A (
+     "rank" INT,
+     demographics_age VARCHAR(50),
+	 demographics_gender VARCHAR(50),
+	 location_citizenship VARCHAR(50),
+	 location_region VARCHAR(50),
+	 wealth_how_industry VARCHAR(50),
+	 wealth_how_inherited VARCHAR(50),
+     PRIMARY KEY ("rank")
+);
+
+CREATE TABLE Billionaires_B (
+     "Name" VARCHAR(50),
+     NetWorth VARCHAR(50),
+	 Country VARCHAR(50),
+	 "Source" VARCHAR(50),
+	 "Rank" INT,
+	 Age VARCHAR(50),
+	 Industry VARCHAR(50)
+);
+
+SELECT * FROM Billionaires_B;
